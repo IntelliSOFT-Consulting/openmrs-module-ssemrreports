6ed211831a162a47e2f894b676374c22b6055d6b
moshonk <omondi.ken@gmail.com>
2023-11-20 07:38:52 +0300

    Removed the OHRI Mamba core dependencies to prevent a frustrating runtime dependency conflict

--- api/src/main/java/org/openmrs/module/ssemrreports/SSEMRReportsActivator.java
+++ api/src/main/java/org/openmrs/module/ssemrreports/SSEMRReportsActivator.java
@@ -9,18 +9,10 @@
  */
 package org.openmrs.module.ssemrreports;
 
-import java.util.Calendar;
-import java.util.UUID;
-
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.openmrs.api.context.Context;
 import org.openmrs.module.BaseModuleActivator;
-import org.openmrs.module.ohrimambacore.task.FlattenTableTask;
 import org.openmrs.module.ssemrreports.reporting.SSEMRReportInitializer;
-import org.openmrs.scheduler.SchedulerException;
-import org.openmrs.scheduler.Task;
-import org.openmrs.scheduler.TaskDefinition;
 
 /**
  * This class contains the logic that is run every time this module is either started or shutdown
@@ -35,9 +27,6 @@ public class SSEMRReportsActivator extends BaseModuleActivator {
 	 * @see #started()
 	 */
 	public void started() {
-		log.info("MambaETL DB Flattening Task registering..");
-		registerTask("MambaETL Task", "MambaETL Task - To Flatten and Prepare Reporting Data.", FlattenTableTask.class,
-		    60 * 60 * 12L, true);
 		try {
 			reportsInitializer.purgeReports();
 			// ssGlobalPropertyService.removeSsGlobalPropertiesEntries("ssemrreports");
@@ -55,39 +44,6 @@ public class SSEMRReportsActivator extends BaseModuleActivator {
 	 */
 	public void shutdown() {
 		log.info("Shutdown SSEMR Reports");
-	}
-	
-	private static boolean registerTask(String name, String description, Class<? extends Task> clazz, long interval,
-	        boolean startOnStartup) {
-		try {
-			Context.addProxyPrivilege("Manage Scheduler");
-			
-			TaskDefinition taskDef = Context.getSchedulerService().getTaskByName(name);
-			if (taskDef == null) {
-				Calendar cal = Calendar.getInstance();
-				cal.add(Calendar.MINUTE, 20);
-				taskDef = new TaskDefinition();
-				taskDef.setTaskClass(clazz.getCanonicalName());
-				taskDef.setStartOnStartup(startOnStartup);
-				taskDef.setRepeatInterval(interval);
-				taskDef.setStarted(true);
-				taskDef.setStartTime(cal.getTime());
-				taskDef.setName(name);
-				taskDef.setUuid(UUID.randomUUID().toString());
-				taskDef.setDescription(description);
-				Context.getSchedulerService().scheduleTask(taskDef);
-				//Context.getSchedulerService().saveTaskDefinition(taskDef);
-			}
-			System.out.println("A Task '" + name + "' has been Registered Successfully!");
-		}
-		catch (SchedulerException ex) {
-			log.warn("Unable to register task '" + name + "' with scheduler", ex);
-			return false;
-		}
-		finally {
-			Context.removeProxyPrivilege("Manage Scheduler");
-		}
-		return true;
 	}
 	
 }
