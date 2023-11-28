7ccefe95964d6ec64a823873e7fefe664fdbb71d
CynthiaKamau <cynthiakamau54@gmail.com>
2023-11-28 22:30:24 +0300

    Add register for listed family members who have taken HIV test and received HIV results

---
+++ api/src/main/java/org/openmrs/module/ssemrreports/reporting/library/queries/ContactsQueries.java
@@ -0,0 +1,18 @@
+package org.openmrs.module.ssemrreports.reporting.library.queries;
+
+import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class ContactsQueries {
+	
+	public static String getContactsWithUnknownHivStatusTested() {
+		
+		String query = "SELECT client_id FROM ssemr_etl.ssemr_flat_encounter_hiv_care_enrolment "
+		        + " WHERE family_member_names != '' AND family_member_names IS NOT NULL "
+		        + " AND family_member_hiv_status = 'Don''t Know' and encounter_datetime between :startDate and "
+		        + " :endDate and location_id =:location group by client_id";
+		
+		return query;
+	}
+}
