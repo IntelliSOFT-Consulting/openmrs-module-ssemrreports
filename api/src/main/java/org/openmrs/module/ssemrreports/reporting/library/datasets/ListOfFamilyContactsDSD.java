df9f7714073cd9894e2c34f933a8ac86ca1a8619
Nicholas Ingosi <nicotwendelee@gmail.com>
2023-11-29 23:55:39 +0300

    Add the dataset definition to general table pickup

--- api/src/main/java/org/openmrs/module/ssemrreports/reporting/library/datasets/ListOfFamilyContactsDSD.java
+++ api/src/main/java/org/openmrs/module/ssemrreports/reporting/library/datasets/ListOfFamilyContactsDSD.java
@@ -9,26 +9,36 @@ public class ListOfFamilyContactsDSD extends SSEMRBaseDataSet {
 	
 	public DataSetDefinition getWithUnknownHivStatus() {
 		SqlDataSetDefinition sqlDataSetDefinition = new SqlDataSetDefinition();
+		sqlDataSetDefinition.setName("List of Family contacts with unknown HIV status");
+		sqlDataSetDefinition.setSqlQuery("SELECT * FROM ssemr_etl.ssemr_flat_encounter_family_history");
 		return sqlDataSetDefinition;
 	}
 	
 	public DataSetDefinition getWithUnknownHivStatusTested() {
 		SqlDataSetDefinition sqlDataSetDefinition = new SqlDataSetDefinition();
+		sqlDataSetDefinition.setName("List of Family contacts with unknown HIV status tested");
+		sqlDataSetDefinition.setSqlQuery("SELECT * FROM ssemr_etl.ssemr_flat_encounter_family_history");
 		return sqlDataSetDefinition;
 	}
 	
 	public DataSetDefinition getTestedPositive() {
 		SqlDataSetDefinition sqlDataSetDefinition = new SqlDataSetDefinition();
+		sqlDataSetDefinition.setName("List of Family contacts newly tested positive");
+		sqlDataSetDefinition.setSqlQuery("SELECT * FROM ssemr_etl.ssemr_flat_encounter_family_history");
 		return sqlDataSetDefinition;
 	}
 	
 	public DataSetDefinition getKnownHivPositiveAtStartOfArt() {
 		SqlDataSetDefinition sqlDataSetDefinition = new SqlDataSetDefinition();
+		sqlDataSetDefinition.setName("List of Family contacts known HIV positive at start of ART");
+		sqlDataSetDefinition.setSqlQuery("SELECT * FROM ssemr_etl.ssemr_flat_encounter_family_history");
 		return sqlDataSetDefinition;
 	}
 	
 	public DataSetDefinition getNewlyTestedHivPositiveAndLinkedToArt() {
 		SqlDataSetDefinition sqlDataSetDefinition = new SqlDataSetDefinition();
+		sqlDataSetDefinition.setName("List of Family contacts newly tested HIV positive and linked to ART");
+		sqlDataSetDefinition.setSqlQuery("SELECT * FROM ssemr_etl.ssemr_flat_encounter_family_history");
 		return sqlDataSetDefinition;
 	}
 }
