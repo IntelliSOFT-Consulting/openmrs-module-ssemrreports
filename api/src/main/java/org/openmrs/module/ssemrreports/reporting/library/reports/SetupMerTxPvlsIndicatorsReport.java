0ce1ceb08181badae69882a8a68b769ea3124664
Nicholas Ingosi <nicotwendelee@gmail.com>
2024-02-03 14:15:43 +0300

    Provide the various datasets based on the quaters of the year

--- api/src/main/java/org/openmrs/module/ssemrreports/reporting/library/reports/SetupMerTxPvlsIndicatorsReport.java
+++ api/src/main/java/org/openmrs/module/ssemrreports/reporting/library/reports/SetupMerTxPvlsIndicatorsReport.java
@@ -1,6 +1,8 @@
 package org.openmrs.module.ssemrreports.reporting.library.reports;
 
+import org.openmrs.Location;
 import org.openmrs.module.reporting.ReportingException;
+import org.openmrs.module.reporting.evaluation.parameter.Parameter;
 import org.openmrs.module.reporting.report.ReportDesign;
 import org.openmrs.module.reporting.report.definition.ReportDefinition;
 import org.openmrs.module.ssemrreports.manager.SSEMRDataExportManager;
@@ -11,6 +13,7 @@ import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
 @Component
@@ -46,12 +49,22 @@ public class SetupMerTxPvlsIndicatorsReport extends SSEMRDataExportManager {
 	@Override
 	public ReportDefinition constructReportDefinition() {
 		ReportDefinition rd = new ReportDefinition();
-		String mappings = "startDate=${startDate},endDate=${endDate+23h},location=${location}";
+		String mappings = "startDate=${endDate-12m},endDate=${endDate},location=${location}";
+		String mappingsQ4 = "startDate=${endDate-9m},endDate=${endDate},location=${location}";
+		String mappingsQ3 = "startDate=${endDate-6m},endDate=${endDate-3m},location=${location}";
+		String mappingsQ2 = "startDate=${endDate-3m},endDate=${endDate-6m},location=${location}";
+		String mappingsQ1 = "startDate=${startDate-12m},endDate=${endDate-9m},location=${location}";
 		rd.setUuid(getUuid());
 		rd.setName(getName());
+		rd.addParameter(new Parameter("endDate", "End Date", Date.class));
+		rd.addParameter(new Parameter("location", "Facility", Location.class));
 		rd.setDescription(getDescription());
-		rd.addParameters(merIndicatorsDatasetDefinition.getParameters());
+		//rd.addParameters(merIndicatorsDatasetDefinition.getParameters());
 		rd.addDataSetDefinition("TxP", SSEMRReportUtils.map(merIndicatorsDatasetDefinition.getTxPvlsDataset(), mappings));
+		rd.addDataSetDefinition("TxPQ1", SSEMRReportUtils.map(merIndicatorsDatasetDefinition.getTxPvlsDataset(), mappingsQ1));
+		rd.addDataSetDefinition("TxPQ2", SSEMRReportUtils.map(merIndicatorsDatasetDefinition.getTxPvlsDataset(), mappingsQ2));
+		rd.addDataSetDefinition("TxPQ3", SSEMRReportUtils.map(merIndicatorsDatasetDefinition.getTxPvlsDataset(), mappingsQ3));
+		rd.addDataSetDefinition("TxPQ4", SSEMRReportUtils.map(merIndicatorsDatasetDefinition.getTxPvlsDataset(), mappingsQ4));
 		return rd;
 	}
 	
