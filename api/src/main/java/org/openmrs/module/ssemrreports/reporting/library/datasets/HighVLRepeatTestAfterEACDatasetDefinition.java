118be3958ea3372c78fefe5cc7695554531aa367
moshonk <omondi.ken@gmail.com>
2024-02-23 08:15:25 +0300

    Updated template for "Patient high VL who received repeat test after EAC list on Date"

--- api/src/main/java/org/openmrs/module/ssemrreports/reporting/library/datasets/HighVLRepeatTestAfterEACDatasetDefinition.java
+++ api/src/main/java/org/openmrs/module/ssemrreports/reporting/library/datasets/HighVLRepeatTestAfterEACDatasetDefinition.java
@@ -4,6 +4,7 @@ import java.util.Date;
 import org.openmrs.Location;
 
 import org.openmrs.PatientIdentifierType;
+import org.openmrs.PersonAttributeType;
 import org.openmrs.api.context.Context;
 import org.openmrs.module.reporting.common.SortCriteria;
 import org.openmrs.module.reporting.data.DataDefinition;
@@ -17,10 +18,24 @@ import org.openmrs.module.reporting.data.person.definition.AgeDataDefinition;
 import org.openmrs.module.reporting.data.person.definition.BirthdateDataDefinition;
 import org.openmrs.module.reporting.data.person.definition.ConvertedPersonDataDefinition;
 import org.openmrs.module.reporting.data.person.definition.GenderDataDefinition;
+import org.openmrs.module.reporting.data.person.definition.PersonAttributeDataDefinition;
 import org.openmrs.module.reporting.data.person.definition.PreferredNameDataDefinition;
 import org.openmrs.module.reporting.dataset.definition.DataSetDefinition;
 import org.openmrs.module.reporting.dataset.definition.PatientDataSetDefinition;
 import org.openmrs.module.reporting.evaluation.parameter.Parameter;
+import org.openmrs.module.ssemrreports.reporting.calculation.BomaAddressCalculation;
+import org.openmrs.module.ssemrreports.reporting.calculation.LandmarkAddressCalculation;
+import org.openmrs.module.ssemrreports.reporting.calculation.PayamAddressCalculation;
+import org.openmrs.module.ssemrreports.reporting.converter.CalculationResultConverter;
+import org.openmrs.module.ssemrreports.reporting.library.data.converter.PersonAttributeDataConverter;
+import org.openmrs.module.ssemrreports.reporting.library.data.definition.BreastFeedingDataDefinition;
+import org.openmrs.module.ssemrreports.reporting.library.data.definition.COVNameDataDefinition;
+import org.openmrs.module.ssemrreports.reporting.library.data.definition.CalculationDataDefinition;
+import org.openmrs.module.ssemrreports.reporting.library.data.definition.LastVLDataDefinition;
+import org.openmrs.module.ssemrreports.reporting.library.data.definition.LastVLTestDateDataDefinition;
+import org.openmrs.module.ssemrreports.reporting.library.data.definition.LinkedToCOVDataDefinition;
+import org.openmrs.module.ssemrreports.reporting.library.data.definition.PregnantDataDefinition;
+import org.openmrs.module.ssemrreports.reporting.library.data.definition.VLDueDateDataDefinition;
 import org.openmrs.module.ssemrreports.reporting.utils.constants.reports.shared.SharedReportConstants;
 import org.springframework.stereotype.Component;
 import org.openmrs.module.metadatadeploy.MetadataUtils;
@@ -28,6 +43,20 @@ import org.openmrs.module.metadatadeploy.MetadataUtils;
 @Component
 public class HighVLRepeatTestAfterEACDatasetDefinition extends SSEMRBaseDataSet {
 	
+	private DataDefinition personPayamAddress() {
+		CalculationDataDefinition cd = new CalculationDataDefinition("payam", new PayamAddressCalculation());
+		return cd;
+	}
+	
+	private DataDefinition personBomaAddress() {
+		CalculationDataDefinition cd = new CalculationDataDefinition("boma", new BomaAddressCalculation());
+		return cd;
+	}
+	
+	private DataDefinition personLandmarkAddress() {
+		CalculationDataDefinition cd = new CalculationDataDefinition("landmark", new LandmarkAddressCalculation());
+		return cd;
+	}
 	public DataSetDefinition constructHighVLRepeatTestAfterEACDatasetDefinition() {
 		
 		String DATE_FORMAT = "dd-MMM-yyyy";
@@ -42,10 +71,34 @@ public class HighVLRepeatTestAfterEACDatasetDefinition extends SSEMRBaseDataSet 
 		DataConverter nameFormatter = new ObjectFormatter("{familyName} {givenName} {middleName}");
 		DataDefinition nameDef = new ConvertedPersonDataDefinition("name", new PreferredNameDataDefinition(), nameFormatter);
 		PatientIdentifierType openmrsID = Context.getPatientService().getPatientIdentifierTypeByUuid(
-		    SharedReportConstants.UNIQUE_ART_NUMBER_TYPE_UUID);
+				SharedReportConstants.UNIQUE_ART_NUMBER_TYPE_UUID);
 		DataConverter identifierFormatter = new ObjectFormatter("{identifier}");
 		DataDefinition identifierDef = new ConvertedPatientDataDefinition("identifier", new PatientIdentifierDataDefinition(
-		        openmrsID.getName(), openmrsID), identifierFormatter);
+				openmrsID.getName(), openmrsID), identifierFormatter);
+		
+		PersonAttributeType phoneNumber = Context.getPersonService().getPersonAttributeTypeByUuid(
+				SharedReportConstants.PHONE_NUMBER_ATTRIBUTE_TYPE_UUID);
+		
+		COVNameDataDefinition covNameDataDefinition = new COVNameDataDefinition();
+		covNameDataDefinition.addParameter(new Parameter("endDate", "End Date", Date.class));
+		
+		LinkedToCOVDataDefinition linkedToCOVDataDefinition = new LinkedToCOVDataDefinition();
+		linkedToCOVDataDefinition.addParameter(new Parameter("endDate", "End Date", Date.class));
+		
+		LastVLDataDefinition lastVLDataDefinition = new LastVLDataDefinition();
+		lastVLDataDefinition.addParameter(new Parameter("endDate", "End Date", Date.class));
+		
+		LastVLTestDateDataDefinition lastVLTestDateDataDefinition = new LastVLTestDateDataDefinition();
+		lastVLTestDateDataDefinition.addParameter(new Parameter("endDate", "End Date", Date.class));
+		
+		VLDueDateDataDefinition vlDueDateDataDefinition = new VLDueDateDataDefinition();
+		vlDueDateDataDefinition.addParameter(new Parameter("endDate", "End Date", Date.class));
+		
+		PregnantDataDefinition pregnantDataDefinition = new PregnantDataDefinition();
+		pregnantDataDefinition.addParameter(new Parameter("endDate", "End Date", Date.class));
+		
+		BreastFeedingDataDefinition breastfeedingDataDefinition = new BreastFeedingDataDefinition();
+		breastfeedingDataDefinition.addParameter(new Parameter("endDate", "End Date", Date.class));
 		
 		dsd.addColumn("id", new PatientIdDataDefinition(), "");
 		dsd.addColumn("Identifier", identifierDef, (String) null);
@@ -53,6 +106,17 @@ public class HighVLRepeatTestAfterEACDatasetDefinition extends SSEMRBaseDataSet 
 		dsd.addColumn("DOB", new BirthdateDataDefinition(), "", new BirthdateConverter(DATE_FORMAT));
 		dsd.addColumn("Age", new AgeDataDefinition(), "", null);
 		dsd.addColumn("Gender", new GenderDataDefinition(), "", null);
+		dsd.addColumn("Telephone", new PersonAttributeDataDefinition("Phone Number", phoneNumber), "",
+				new PersonAttributeDataConverter());
+		dsd.addColumn("Pregnant", pregnantDataDefinition, "endDate=${endDate}");
+		dsd.addColumn("Breastfeeding", breastfeedingDataDefinition, "endDate=${endDate}");
+		dsd.addColumn("Date of last VL test", lastVLTestDateDataDefinition, "endDate=${endDate}");
+		dsd.addColumn("VL due date", vlDueDateDataDefinition, "endDate=${endDate}");
+		dsd.addColumn("Payam", personPayamAddress(), "", new CalculationResultConverter());
+		dsd.addColumn("Boma", personBomaAddress(), "", new CalculationResultConverter());
+		dsd.addColumn("Landmark", personLandmarkAddress(), "", new CalculationResultConverter());
+		dsd.addColumn("Name of COV", covNameDataDefinition, "endDate=${endDate}");
+		dsd.addColumn("Linked to COV (Y/N)", linkedToCOVDataDefinition, "endDate=${endDate}");
 		
 		return dsd;
 	}
