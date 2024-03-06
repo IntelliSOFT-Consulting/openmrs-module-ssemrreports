f7aaa3e252c04a6dadf388946e86e9d72467b354
moshonk <omondi.ken@gmail.com>
2024-03-07 00:09:59 +0300

    Added columns the HVL report

---
+++ api/src/main/java/org/openmrs/module/ssemrreports/reporting/library/data/evaluator/LastEAC3DateDataEvaluator.java
@@ -0,0 +1,54 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
+ * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
+ *
+ * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
+ * graphic logo is a trademark of OpenMRS Inc.
+ */
+package org.openmrs.module.ssemrreports.reporting.library.data.evaluator;
+
+import java.util.Date;
+import java.util.Map;
+
+import org.openmrs.annotation.Handler;
+import org.openmrs.module.reporting.data.person.EvaluatedPersonData;
+import org.openmrs.module.reporting.data.person.definition.PersonDataDefinition;
+import org.openmrs.module.reporting.data.person.evaluator.PersonDataEvaluator;
+import org.openmrs.module.reporting.evaluation.EvaluationContext;
+import org.openmrs.module.reporting.evaluation.EvaluationException;
+import org.openmrs.module.reporting.evaluation.querybuilder.SqlQueryBuilder;
+import org.openmrs.module.reporting.evaluation.service.EvaluationService;
+import org.openmrs.module.ssemrreports.reporting.library.data.definition.LastEAC3DateDataDefinition;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * Evaluates Last EAC3 Date Data Definition
+ */
+@Handler(supports = LastEAC3DateDataDefinition.class, order = 50)
+public class LastEAC3DateDataEvaluator implements PersonDataEvaluator {
+	
+	@Autowired
+	private EvaluationService evaluationService;
+	
+	public EvaluatedPersonData evaluate(PersonDataDefinition definition, EvaluationContext context)
+	        throws EvaluationException {
+		EvaluatedPersonData c = new EvaluatedPersonData(definition, context);
+		
+		String qry = "SELECT client_id,max(encounter_datetime) as lastEac3Date FROM ssemr_flat_encounter_high_viral_load "
+		        + " WHERE  date(encounter_datetime) <= date(:endDate) " + " AND eac_session = 'Third EAC Session' "
+		        + " GROUP BY client_id;";
+		
+		SqlQueryBuilder queryBuilder = new SqlQueryBuilder();
+		queryBuilder.append(qry);
+		Date startDate = (Date) context.getParameterValue("startDate");
+		Date endDate = (Date) context.getParameterValue("endDate");
+		queryBuilder.addParameter("endDate", endDate);
+		queryBuilder.addParameter("startDate", startDate);
+		
+		Map<Integer, Object> data = evaluationService.evaluateToMap(queryBuilder, Integer.class, Object.class, context);
+		c.setData(data);
+		return c;
+	}
+}
