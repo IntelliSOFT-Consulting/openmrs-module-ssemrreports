5103f9777fbab2eb44192cb41696a3c08bd42e4d
moshonk <omondi.ken@gmail.com>
2024-03-07 22:24:07 +0300

    Query update to format date

--- api/src/main/java/org/openmrs/module/ssemrreports/reporting/library/data/evaluator/LastEAC2DateDataEvaluator.java
+++ api/src/main/java/org/openmrs/module/ssemrreports/reporting/library/data/evaluator/LastEAC2DateDataEvaluator.java
@@ -36,7 +36,7 @@ public class LastEAC2DateDataEvaluator implements PersonDataEvaluator {
 	        throws EvaluationException {
 		EvaluatedPersonData c = new EvaluatedPersonData(definition, context);
 		
-		String qry = "SELECT client_id,max(encounter_datetime) as lastEac2Date FROM ssemr_etl.ssemr_flat_encounter_high_viral_load "
+		String qry = "SELECT client_id,DATE(max(encounter_datetime)) as lastEac2Date FROM ssemr_etl.ssemr_flat_encounter_high_viral_load "
 		        + " WHERE  date(encounter_datetime) <= date(:endDate) "
 		        + " AND eac_session = 'Second EAC Session' "
 		        + " GROUP BY client_id;";
