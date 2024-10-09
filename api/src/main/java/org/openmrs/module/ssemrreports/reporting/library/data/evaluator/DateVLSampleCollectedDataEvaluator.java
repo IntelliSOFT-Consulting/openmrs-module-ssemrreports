2dc31270e4d9242fbc26711258e2f0b0c06354d2
Michael Ndula <53141613+Michaelndula@users.noreply.github.com>
2024-10-09 09:59:42 +0300

    SSEMR-235 Removimg timestamps in the Date Viral Load sample collected culumn (#94)

--- api/src/main/java/org/openmrs/module/ssemrreports/reporting/library/data/evaluator/DateVLSampleCollectedDataEvaluator.java
+++ api/src/main/java/org/openmrs/module/ssemrreports/reporting/library/data/evaluator/DateVLSampleCollectedDataEvaluator.java
@@ -36,7 +36,7 @@ public class DateVLSampleCollectedDataEvaluator implements PersonDataEvaluator {
 	        throws EvaluationException {
 		EvaluatedPersonData c = new EvaluatedPersonData(definition, context);
 		
-		String qry = "SELECT client_id, DATE_FORMAT(max(date_vl_sample_collected), '%Y-%m-%d %H:%i:%s') as documented_date FROM ssemr_etl.ssemr_flat_encounter_hiv_care_follow_up where DATE(encounter_datetime) <= :endDate group by client_id";
+		String qry = "SELECT client_id, DATE_FORMAT(max(date_vl_sample_collected), '%Y-%m-%d') as documented_date FROM ssemr_etl.ssemr_flat_encounter_hiv_care_follow_up where DATE(encounter_datetime) <= :endDate group by client_id";
 		
 		SqlQueryBuilder queryBuilder = new SqlQueryBuilder();
 		queryBuilder.append(qry);
