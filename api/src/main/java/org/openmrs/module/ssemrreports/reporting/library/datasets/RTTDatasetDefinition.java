7c565e0320d22152a4ba985fb630bccd4d081799
Your Name <michaelndula@gmail.com>
2024-10-18 02:08:41 +0300

    SSEMR-345 Separate Line list for IIT and RTT clients report to 2 different Reports 1. Line list for IIT and 2. Line list for RTT

---
+++ api/src/main/java/org/openmrs/module/ssemrreports/reporting/library/datasets/RTTDatasetDefinition.java
@@ -0,0 +1,118 @@
+package org.openmrs.module.ssemrreports.reporting.library.datasets;
+
+import org.openmrs.Location;
+import org.openmrs.PatientIdentifierType;
+import org.openmrs.PersonAttributeType;
+import org.openmrs.api.context.Context;
+import org.openmrs.module.reporting.common.SortCriteria;
+import org.openmrs.module.reporting.data.DataDefinition;
+import org.openmrs.module.reporting.data.converter.DataConverter;
+import org.openmrs.module.reporting.data.converter.ObjectFormatter;
+import org.openmrs.module.reporting.data.patient.definition.ConvertedPatientDataDefinition;
+import org.openmrs.module.reporting.data.patient.definition.PatientIdDataDefinition;
+import org.openmrs.module.reporting.data.patient.definition.PatientIdentifierDataDefinition;
+import org.openmrs.module.reporting.data.person.definition.*;
+import org.openmrs.module.reporting.dataset.definition.DataSetDefinition;
+import org.openmrs.module.reporting.dataset.definition.PatientDataSetDefinition;
+import org.openmrs.module.reporting.evaluation.parameter.Parameter;
+import org.openmrs.module.ssemrreports.reporting.calculation.BomaAddressCalculation;
+import org.openmrs.module.ssemrreports.reporting.calculation.PayamAddressCalculation;
+import org.openmrs.module.ssemrreports.reporting.converter.CalculationResultConverter;
+import org.openmrs.module.ssemrreports.reporting.library.data.converter.PersonAttributeDataConverter;
+import org.openmrs.module.ssemrreports.reporting.library.data.definition.*;
+import org.openmrs.module.ssemrreports.reporting.utils.constants.reports.shared.SharedReportConstants;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class RTTDatasetDefinition extends SsemrBaseDataSet {
+	
+	private DataDefinition personPayamAddress() {
+		CalculationDataDefinition cd = new CalculationDataDefinition("payam", new PayamAddressCalculation());
+		return cd;
+	}
+	
+	private DataDefinition personBomaAddress() {
+		CalculationDataDefinition cd = new CalculationDataDefinition("boma", new BomaAddressCalculation());
+		return cd;
+	}
+	
+	public DataSetDefinition constructRTTDatasetDefinition() {
+		
+		String DATE_FORMAT = "dd-MMM-yyyy";
+		PatientDataSetDefinition dsd = new PatientDataSetDefinition();
+		dsd.setName("RTT");
+		dsd.addParameters(getParameters());
+		dsd.setDescription("Patients who are RTT");
+		dsd.addSortCriteria("Psn", SortCriteria.SortDirection.ASC);
+		dsd.addParameter(new Parameter("location", "Location", Location.class));
+		dsd.addParameter(new Parameter("startDate", "Start Date", Date.class));
+		dsd.addParameter(new Parameter("endDate", "End Date", Date.class));
+		DataConverter nameFormatter = new ObjectFormatter("{givenName} {middleName} {familyName}");
+		DataDefinition nameDef = new ConvertedPersonDataDefinition("name", new PreferredNameDataDefinition(), nameFormatter);
+		
+		PatientIdentifierType openmrsID = Context.getPatientService().getPatientIdentifierTypeByUuid(
+		    SharedReportConstants.UNIQUE_ART_NUMBER_TYPE_UUID);
+		DataConverter identifierFormatter = new ObjectFormatter("{identifier}");
+		DataDefinition identifierDef = new ConvertedPatientDataDefinition("identifier", new PatientIdentifierDataDefinition(
+		        openmrsID.getName(), openmrsID), identifierFormatter);
+		
+		PersonAttributeType phoneNumber = Context.getPersonService().getPersonAttributeTypeByUuid(
+		    SharedReportConstants.PHONE_NUMBER_ATTRIBUTE_TYPE_UUID);
+		
+		PersonAttributeType alternativePhoneNumber = Context.getPersonService().getPersonAttributeTypeByUuid(
+		    SharedReportConstants.ALTERNATIVE_PHONE_NUMBER_ATTRIBUTE_TYPE_UUID);
+		
+		COVNameDataDefinition covNameDataDefinition = new COVNameDataDefinition();
+		covNameDataDefinition.addParameter(new Parameter("endDate", "End Date", Date.class));
+		
+		LinkedToCOVDataDefinition linkedToCOVDataDefinition = new LinkedToCOVDataDefinition();
+		linkedToCOVDataDefinition.addParameter(new Parameter("endDate", "End Date", Date.class));
+		
+		ETLArtStartDateDataDefinition etlArtStartDateDataDefinition = new ETLArtStartDateDataDefinition();
+		etlArtStartDateDataDefinition.addParameter(new Parameter("endDate", "End Date", Date.class));
+		
+		NextAppointmentDateDataDefinition nextAppointmentDateDataDefinition = new NextAppointmentDateDataDefinition();
+		nextAppointmentDateDataDefinition.addParameter(new Parameter("endDate", "End Date", Date.class));
+		
+		LastDrugVisitDateDataDefinition lastDrugVisitDateDataDefinition = new LastDrugVisitDateDataDefinition();
+		lastDrugVisitDateDataDefinition.addParameter(new Parameter("endDate", "End Date", Date.class));
+		
+		MissedAppointmentDateDataDefinition missedAppointmentDateDataDefinition = new MissedAppointmentDateDataDefinition();
+		missedAppointmentDateDataDefinition.addParameter(new Parameter("endDate", "End Date", Date.class));
+		
+		PregnantDataDefinition pregnantDataDefinition = new PregnantDataDefinition();
+		pregnantDataDefinition.addParameter(new Parameter("endDate", "End Date", Date.class));
+		
+		BreastFeedingDataDefinition breastfeedingDataDefinition = new BreastFeedingDataDefinition();
+		breastfeedingDataDefinition.addParameter(new Parameter("endDate", "End Date", Date.class));
+		
+		Reached28DaysAfterIITDateDataDefinition reached28DaysAfterIITDateDataDefinition = new Reached28DaysAfterIITDateDataDefinition();
+		reached28DaysAfterIITDateDataDefinition.addParameter(new Parameter("endDate", "End Date", Date.class));
+		
+		DaysMissedAppointmentDataDefinition daysMissedAppointmentDataDefinition = new DaysMissedAppointmentDataDefinition();
+		daysMissedAppointmentDataDefinition.addParameter(new Parameter("endDate", "End Date", Date.class));
+		
+		dsd.addColumn("id", new PatientIdDataDefinition(), "");
+		dsd.addColumn("Identifier", identifierDef, (String) null);
+		dsd.addColumn("Name", nameDef, "");
+		dsd.addColumn("Telephone", new PersonAttributeDataDefinition("Phone Number", phoneNumber), "",
+		    new PersonAttributeDataConverter());
+		dsd.addColumn("Alternative telephone", new PersonAttributeDataDefinition("Alternative Phone Number",
+		        alternativePhoneNumber), "", new PersonAttributeDataConverter());
+		dsd.addColumn("Age", new AgeDataDefinition(), "", null);
+		dsd.addColumn("Gender", new GenderDataDefinition(), "", null);
+		dsd.addColumn("Pregnant", pregnantDataDefinition, "endDate=${endDate}");
+		dsd.addColumn("Breastfeeding", breastfeedingDataDefinition, "endDate=${endDate}");
+		dsd.addColumn("Date of ART initiation", etlArtStartDateDataDefinition, "endDate=${endDate}");
+		dsd.addColumn("Date returned to treatment (RTT)", nextAppointmentDateDataDefinition, "endDate=${endDate}");
+		dsd.addColumn("Last date of visit", lastDrugVisitDateDataDefinition, "endDate=${endDate}");
+		dsd.addColumn("Payam", personPayamAddress(), "", new CalculationResultConverter());
+		dsd.addColumn("Boma", personBomaAddress(), "", new CalculationResultConverter());
+		dsd.addColumn("Name of COV", covNameDataDefinition, "endDate=${endDate}");
+		dsd.addColumn("Linked to COV (Y/N)", linkedToCOVDataDefinition, "endDate=${endDate}");
+		
+		return dsd;
+	}
+}
