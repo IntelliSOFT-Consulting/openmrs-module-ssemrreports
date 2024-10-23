9bbf1a0327837641c788e39c3ca69484df1d8d8b
Your Name <michaelndula@gmail.com>
2024-10-23 13:28:12 +0300

    Adding Last Visit Date and Returning to Treatment Date Definition and Evaluator.

--- api/src/main/java/org/openmrs/module/ssemrreports/reporting/library/queries/CommonQueries.java
+++ api/src/main/java/org/openmrs/module/ssemrreports/reporting/library/queries/CommonQueries.java
@@ -323,7 +323,7 @@ public class CommonQueries {
 		        + "FROM openmrs.patient_appointment WHERE location_id =:location GROUP BY patient_id) AS latest_appt ON p.patient_id = latest_appt.patient_id "
 		        + "AND p.start_date_time = latest_appt.max_start_date_time LEFT JOIN ssemr_etl.ssemr_flat_encounter_hiv_care_follow_up e "
 		        + "ON e.client_id = p.patient_id WHERE p.status = 'Missed' AND DATE(e.encounter_datetime) <= DATE(:endDate) "
-		        + "AND DATEDIFF(CURDATE(), p.start_date_time) > 28 ORDER BY  p.patient_id ASC) AS t;";
+		        + "AND DATEDIFF(CURDATE(), p.start_date_time) >= 28 ORDER BY  p.patient_id ASC) AS t;";
 		
 		return query;
 	}
