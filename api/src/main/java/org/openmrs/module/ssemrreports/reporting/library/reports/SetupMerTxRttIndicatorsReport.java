ebc23dfe491bd0e9538337adb4cb0c7301698303
Nicholas Ingosi <nicotwendelee@gmail.com>
2024-11-26 09:18:28 +0300

    Fix the date parameters for the RTT

--- api/src/main/java/org/openmrs/module/ssemrreports/reporting/library/reports/SetupMerTxRttIndicatorsReport.java
+++ api/src/main/java/org/openmrs/module/ssemrreports/reporting/library/reports/SetupMerTxRttIndicatorsReport.java
@@ -54,10 +54,10 @@ public class SetupMerTxRttIndicatorsReport extends SsemrDataExportManager {
 	@Override
 	public ReportDefinition constructReportDefinition() {
 		ReportDefinition rd = new ReportDefinition();
-		String mappings = "startDate=${startDate},endDate=${endDate+23h},location=${location}";
-		String mappings1 = "startDate=${startDate},endDate=${startDate+1m+23h},location=${location}";
-		String mappings2 = "startDate=${startDate+1m},endDate=${startDate+2m+23h},location=${location}";
-		String mappings3 = "startDate=${startDate+2m},endDate=${endDate+23h},location=${location}";
+		String mappings = "startDate=${startDate},endDate=${endDate},location=${location}";
+		String mappings1 = "startDate=${startDate},endDate=${startDate+1m-1d},location=${location}";
+		String mappings2 = "startDate=${startDate+1m},endDate=${startDate+2m-1d},location=${location}";
+		String mappings3 = "startDate=${startDate+2m},endDate=${endDate},location=${location}";
 		rd.setUuid(getUuid());
 		rd.setName(getName());
 		rd.setDescription(getDescription());
