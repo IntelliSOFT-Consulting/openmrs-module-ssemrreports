dc1650f7e3566520483fcdb897c75cb78b207c34
Your Name <michaelndula@gmail.com>
2024-12-11 17:37:23 +0300

    SSEMR-468 (Line Lists) - Split TB Screening and Treatment to two reports 1. Clients on TB Treatment and 2. Clients With TB Screening

--- api/src/main/java/org/openmrs/module/ssemrreports/reporting/library/reports/SetupTbTreatmentRegister.java
+++ api/src/main/java/org/openmrs/module/ssemrreports/reporting/library/reports/SetupTbTreatmentRegister.java
@@ -80,7 +80,9 @@ public class SetupTbTreatmentRegister extends SsemrDataExportManager {
 			props.put("sortWeight", "5000");
 			reportDesign.setProperties(props);
 		}
-		catch (IOException e) { throw new ReportingException("Error creating report design", e); }
+		catch (IOException e) {
+			throw new ReportingException("Error creating report design", e);
+		}
 		
 		return Arrays.asList(reportDesign);
 	}
