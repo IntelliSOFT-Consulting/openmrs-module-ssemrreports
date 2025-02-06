b9b352f3f8f75e53e6e542dda9ffda44b06b7319
Your Name <michaelndula@gmail.com>
2025-02-06 11:12:07 +0300

    Reference branch

---
+++ api/src/main/java/org/openmrs/module/ssemrreports/reporting/library/data/evaluator/ClientRepresentedDataEvaluator.java
@@ -0,0 +1,45 @@
+package org.openmrs.module.ssemrreports.reporting.library.data.evaluator;
+
+import org.openmrs.annotation.Handler;
+import org.openmrs.module.reporting.data.person.EvaluatedPersonData;
+import org.openmrs.module.reporting.data.person.definition.PersonDataDefinition;
+import org.openmrs.module.reporting.data.person.evaluator.PersonDataEvaluator;
+import org.openmrs.module.reporting.evaluation.EvaluationContext;
+import org.openmrs.module.reporting.evaluation.EvaluationException;
+import org.openmrs.module.reporting.evaluation.querybuilder.SqlQueryBuilder;
+import org.openmrs.module.reporting.evaluation.service.EvaluationService;
+import org.openmrs.module.ssemrreports.reporting.library.data.definition.ClientRepresentedDataDefinition;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Date;
+import java.util.Map;
+
+/**
+ * Evaluates Client Represented Data Definition
+ */
+@Handler(supports = ClientRepresentedDataDefinition.class, order = 50)
+public class ClientRepresentedDataEvaluator implements PersonDataEvaluator {
+
+    @Autowired
+    private EvaluationService evaluationService;
+
+    public EvaluatedPersonData evaluate(PersonDataDefinition definition, EvaluationContext context)
+            throws EvaluationException {
+        EvaluatedPersonData c = new EvaluatedPersonData(definition, context);
+
+        String qry = "select client_id, CASE MID(MAX(CONCAT(encounter_datetime, client_represented)), 20) WHEN 'Yes' THEN 'YES' WHEN 'No' THEN 'NO' ELSE 'N/A' END "
+                + " AS represented FROM ssemr_etl.ssemr_flat_encounter_hiv_care_follow_up GROUP BY client_id, edd >= DATE(:endDate)";
+
+        SqlQueryBuilder queryBuilder = new SqlQueryBuilder();
+        queryBuilder.append(qry);
+        Date startDate = (Date) context.getParameterValue("startDate");
+        Date endDate = (Date) context.getParameterValue("endDate");
+        queryBuilder.addParameter("endDate", endDate);
+        queryBuilder.addParameter("startDate", startDate);
+
+        Map<Integer, Object> data = evaluationService.evaluateToMap(queryBuilder, Integer.class, Object.class, context);
+        c.setData(data);
+        return c;
+    }
+
+}
