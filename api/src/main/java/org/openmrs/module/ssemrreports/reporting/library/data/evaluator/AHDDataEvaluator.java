b52f6f958892dd922accd9cdb600a87ca9e2ef8c
moshonk <omondi.ken@gmail.com>
2025-03-18 16:28:59 +0300

    Fixes indentation for code readability
    Corrects the indentation across several data definition and evaluator classes for improved readability and consistency with coding standards.

--- api/src/main/java/org/openmrs/module/ssemrreports/reporting/library/data/evaluator/AHDDataEvaluator.java
+++ api/src/main/java/org/openmrs/module/ssemrreports/reporting/library/data/evaluator/AHDDataEvaluator.java
@@ -19,34 +19,34 @@ import java.util.Map;
  */
 @Handler(supports = AHDDataDefinition.class, order = 50)
 public class AHDDataEvaluator implements PersonDataEvaluator {
-
-    @Autowired
-    private EvaluationService evaluationService;
-
-    public EvaluatedPersonData evaluate(PersonDataDefinition definition, EvaluationContext context)
-            throws EvaluationException {
-        EvaluatedPersonData c = new EvaluatedPersonData(definition, context);
-
-        String qry = "SELECT client_id, " + "CASE "
-                + "  WHEN cd4 < 200 OR who_clinical_stage IN ('WHO Stage 3', 'WHO Stage 4') OR "
-                + "       TIMESTAMPDIFF(YEAR, birthdate, CURDATE()) < 5 OR on_tb_treatment = 'Yes' THEN 'Yes' "
-                + "  ELSE 'No' " + "END AS result " + "FROM ( "
-                + "  SELECT e.client_id, e.cd4, e.who_clinical_stage, e.on_tb_treatment, p.birthdate "
-                + "  FROM ssemr_etl.ssemr_flat_encounter_hiv_care_follow_up e "
-                + "  JOIN ssemr_etl.mamba_dim_person p ON e.client_id = p.person_id "
-                + "  WHERE DATE(e.encounter_datetime) <= DATE(:endDate) " + "  AND e.encounter_datetime = ( "
-                + "    SELECT MAX(encounter_datetime) " + "    FROM ssemr_etl.ssemr_flat_encounter_hiv_care_follow_up "
-                + "    WHERE client_id = e.client_id " + "  ) " + ") AS latest_encounter;";
-
-        SqlQueryBuilder queryBuilder = new SqlQueryBuilder();
-        queryBuilder.append(qry);
-        Date startDate = (Date) context.getParameterValue("startDate");
-        Date endDate = (Date) context.getParameterValue("endDate");
-        queryBuilder.addParameter("endDate", endDate);
-        queryBuilder.addParameter("startDate", startDate);
-
-        Map<Integer, Object> data = evaluationService.evaluateToMap(queryBuilder, Integer.class, Object.class, context);
-        c.setData(data);
-        return c;
-    }
+	
+	@Autowired
+	private EvaluationService evaluationService;
+	
+	public EvaluatedPersonData evaluate(PersonDataDefinition definition, EvaluationContext context)
+	        throws EvaluationException {
+		EvaluatedPersonData c = new EvaluatedPersonData(definition, context);
+		
+		String qry = "SELECT client_id, " + "CASE "
+		        + "  WHEN cd4 < 200 OR who_clinical_stage IN ('WHO Stage 3', 'WHO Stage 4') OR "
+		        + "       TIMESTAMPDIFF(YEAR, birthdate, CURDATE()) < 5 OR on_tb_treatment = 'Yes' THEN 'Yes' "
+		        + "  ELSE 'No' " + "END AS result " + "FROM ( "
+		        + "  SELECT e.client_id, e.cd4, e.who_clinical_stage, e.on_tb_treatment, p.birthdate "
+		        + "  FROM ssemr_etl.ssemr_flat_encounter_hiv_care_follow_up e "
+		        + "  JOIN ssemr_etl.mamba_dim_person p ON e.client_id = p.person_id "
+		        + "  WHERE DATE(e.encounter_datetime) <= DATE(:endDate) " + "  AND e.encounter_datetime = ( "
+		        + "    SELECT MAX(encounter_datetime) " + "    FROM ssemr_etl.ssemr_flat_encounter_hiv_care_follow_up "
+		        + "    WHERE client_id = e.client_id " + "  ) " + ") AS latest_encounter;";
+		
+		SqlQueryBuilder queryBuilder = new SqlQueryBuilder();
+		queryBuilder.append(qry);
+		Date startDate = (Date) context.getParameterValue("startDate");
+		Date endDate = (Date) context.getParameterValue("endDate");
+		queryBuilder.addParameter("endDate", endDate);
+		queryBuilder.addParameter("startDate", startDate);
+		
+		Map<Integer, Object> data = evaluationService.evaluateToMap(queryBuilder, Integer.class, Object.class, context);
+		c.setData(data);
+		return c;
+	}
 }
