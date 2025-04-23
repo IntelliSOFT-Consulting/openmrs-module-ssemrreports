9d7ac69881cb20fe560a80708838ad20ce8b8e43
Your Name <michaelndula@gmail.com>
2025-04-23 10:41:59 +0300

    Populate Patient IDs from 1.* instead of using patient IDs

--- api/src/main/java/org/openmrs/module/ssemrreports/reporting/library/datasets/EligibleForTPTDatasetDefinition.java
+++ api/src/main/java/org/openmrs/module/ssemrreports/reporting/library/datasets/EligibleForTPTDatasetDefinition.java
@@ -19,15 +19,25 @@ import org.openmrs.module.reporting.data.person.definition.PreferredNameDataDefi
 import org.openmrs.module.reporting.dataset.definition.DataSetDefinition;
 import org.openmrs.module.reporting.dataset.definition.PatientDataSetDefinition;
 import org.openmrs.module.reporting.evaluation.parameter.Parameter;
-import org.openmrs.module.ssemrreports.reporting.library.data.definition.*;
+import org.openmrs.module.ssemrreports.reporting.library.data.definition.LinkedToCOVDataDefinition;
+import org.openmrs.module.ssemrreports.reporting.library.data.definition.COVNameDataDefinition;
+import org.openmrs.module.ssemrreports.reporting.calculation.PayamAddressCalculation;
+import org.openmrs.module.ssemrreports.reporting.calculation.BomaAddressCalculation;
+import org.openmrs.module.ssemrreports.reporting.library.data.definition.ETLArtStartDateDataDefinition;
+import org.openmrs.module.ssemrreports.reporting.library.data.definition.TPTStartDateDataDefinition;
+import org.openmrs.module.ssemrreports.reporting.library.data.definition.ClientOnTPTDataDefinition;
+import org.openmrs.module.ssemrreports.reporting.library.data.definition.TPTEligibleDateDataDefinition;
+import org.openmrs.module.ssemrreports.reporting.library.data.definition.CalculationDataDefinition;
+import org.openmrs.module.ssemrreports.reporting.converter.CalculationResultConverter;
+import org.openmrs.module.ssemrreports.reporting.library.data.definition.PregnantDataDefinition;
+import org.openmrs.module.ssemrreports.reporting.library.data.definition.TPTCompletedDataDefinition;
+import org.openmrs.module.ssemrreports.reporting.library.data.definition.BreastFeedingDataDefinition;
+import org.openmrs.module.ssemrreports.reporting.library.data.definition.IndexDataDefinition;
 import org.openmrs.module.ssemrreports.reporting.utils.constants.reports.shared.SharedReportConstants;
 import org.springframework.stereotype.Component;
 import org.openmrs.module.reporting.data.person.definition.PersonAttributeDataDefinition;
 import org.openmrs.module.ssemrreports.reporting.library.data.converter.PersonAttributeDataConverter;
 import org.openmrs.PersonAttributeType;
-import org.openmrs.module.ssemrreports.reporting.calculation.PayamAddressCalculation;
-import org.openmrs.module.ssemrreports.reporting.calculation.BomaAddressCalculation;
-import org.openmrs.module.ssemrreports.reporting.converter.CalculationResultConverter;
 import org.openmrs.module.ssemrreports.reporting.calculation.LandmarkAddressCalculation;
 
 @Component
