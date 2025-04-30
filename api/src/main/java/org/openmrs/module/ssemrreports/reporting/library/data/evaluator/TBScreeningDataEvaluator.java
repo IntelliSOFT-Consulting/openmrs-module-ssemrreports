d9ada49764986c594016170caf8e70d5cd88cfdf
Michael Ndula <53141613+Michaelndula@users.noreply.github.com>
2025-04-30 12:21:18 +0300

    (fix) Get the correct TB Status and Determine if client is screened for TB (#130)
    * (fix) Get the correct TB Status and Determine if client is screened for TB
    
    * (fix) Resolve PR Comments/suggestions

--- api/src/main/java/org/openmrs/module/ssemrreports/reporting/library/data/evaluator/TBScreeningDataEvaluator.java
+++ api/src/main/java/org/openmrs/module/ssemrreports/reporting/library/data/evaluator/TBScreeningDataEvaluator.java
@@ -26,9 +26,14 @@ public class TBScreeningDataEvaluator implements PersonDataEvaluator {
 	public EvaluatedPersonData evaluate(PersonDataDefinition definition, EvaluationContext context)
 	        throws EvaluationException {
 		EvaluatedPersonData c = new EvaluatedPersonData(definition, context);
-		
-		String qry = "select client_id, CASE MID(MAX(CONCAT(encounter_datetime, on_tb_treatment)), 20) WHEN 'Yes' THEN 'YES' WHEN 'No' THEN 'NO' END "
-		        + " AS tb_screening FROM ssemr_etl.ssemr_flat_encounter_hiv_care_follow_up GROUP BY client_id";
+
+		String qry = "SELECT client_id, " + "CASE "
+				+ "WHEN MID(MAX(CONCAT(encounter_datetime, on_tb_treatment)), 20) = 'No' "
+				+ "	 OR MID(MAX(CONCAT(encounter_datetime, tb_status)), 20) IN ('No Signs', 'Pr TB - Presumptive TB') "
+				+ "THEN 'YES' " + "WHEN MID(MAX(CONCAT(encounter_datetime, on_tb_treatment)), 20) = 'Yes' THEN 'NO' "
+				+ "WHEN MID(MAX(CONCAT(encounter_datetime, on_tb_treatment)), 20) IS NULL THEN 'N/A' "
+				+ "END AS tb_screening " + "FROM ssemr_etl.ssemr_flat_encounter_hiv_care_follow_up "
+				+ "WHERE encounter_datetime BETWEEN :startDate AND :endDate " + "GROUP BY client_id";
 		
 		SqlQueryBuilder queryBuilder = new SqlQueryBuilder();
 		queryBuilder.append(qry);
